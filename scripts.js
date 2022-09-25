68e58ffc7484b05c3efe3750ea7a21a2e83514c3
Josh Faigan <djjoker13@gmail.com>
2022-09-25 14:03:35 -0400

    cart fully functional

--- scripts.js
+++ scripts.js
@@ -46,17 +46,44 @@ all_items_button.forEach(elt => elt.addEventListener('click', () => {
   }))
 const cart = []
 // --------------------------------------------------
+// Handle change events on update input
+itemList.onchange = function (e) {
+	if (e.target && e.target.classList.contains('update')) {
+		const name = e.target.dataset.name
+		const qty = parseInt(e.target.value)
+		updateCart(name, qty)
+	}
+}
+// --------------------------------------------------
+// Handle clicks on list
+itemList.onclick = function(e) {
+	// console.log('Clicked List')
+	// console.log(e.target)
+	if (e.target && e.target.classList.contains('remove')) {
+		const name = e.target.dataset.name // data-name="????"
+		removeItem(name)
+	} else if (e.target && e.target.classList.contains('add-one')) {
+		const name = e.target.dataset.name
+		addItem(name)
+	} else if (e.target && e.target.classList.contains('remove-one')) {
+		const name = e.target.dataset.name
+		removeItem(name, 1)
+	}
+}
+// --------------------------------------------------
 // Add Item
 function addItem(name, price) {
 	for (let i = 0; i <cart.length; i++) {
 		if (cart[i].name === name) {
 			cart[i].qty += 1
+			showItems()
 			return
 		}
 	}
 	
 	const item = {name, price, qty: 1}
 	cart.push(item)
+	showItems()
 }
 // ---------------------------------------------------
 // Show Items with variable added (const qty = getQty())
@@ -75,12 +102,18 @@ function showItems(item) {
 		//{name: 'Apple', price : 0.99, qty: 3}
 		const {name, price, qty} = cart[i]
 
-		itemStr += `<li>${name} $${price} x ${qty} = ${qty * price}</li>`
+		itemStr += `<li>
+		${name} $${price} x ${qty} = ${qty * price} 
+		<button class="remove" data-name="${name}">Remove Item</button>
+		<button class="add-one" data-name="${name}"> + </button>
+		<button class="remove-one" data-name="${name}"> - </button>
+		<input class="update" type="number" date-name="${name}">
+		</li>`
 	}
 	itemList.innerHTML = itemStr
 	// show item without variable added
 	//console.log(`Total in cart: $${getTotal()}`)
-	cartTotal.innerHTML = `Total in cart: $${getTotal()}`
+	cartTotal.innerHTML = `Total in cart: $${getTotal()}`	
 }
 
 // -----------------------------------------------------
@@ -114,11 +147,27 @@ function removeItem(name, qty = 0) {
 			if (cart[i].qty < 1 || qty === 0) {
 				cart.splice(i, 1)
 			}
+			showItems()
 			return
 		}
 	}
 }
 
+// --------------------------------------------------------
+function updateCart(name, qty) {
+	for (let i = 0; i < cart.length; i++) {
+		if (cart[i].name === name) {
+			if (qty < 1) {
+				removeItem(name)
+				return
+			}
+			cart[i].qty = qty
+			showItems()
+			return
+		}
+	}
+}
+// --------------------------------------------------------
 // addItem('Apple', 0.99)
 // addItem('Orange', 1.29)
 // addItem('Cucumber', 2.00)
